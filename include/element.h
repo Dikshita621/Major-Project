8754d623faea637068cebbd1873fb4e7e8b40d2c
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-11-29 15:17:43 +0530

    Update element.h

--- include/element.h
+++ include/element.h
@@ -2,13 +2,13 @@
 #define ELEMENT_H
 
 typedef struct {
-    int atomicNumber;
-    char name[20];
-    char symbol[3];
-    char electronConfig[50];
+    int Atomic_Number;
+    char Name[20];
+    char Symbol[3];
+    char Electro_Config[50];
 } Element;
 
-void printElementInfo(Element e);
+void printElementInfo(Element E);
 int searchByAtomicNumber(int number);
 int searchByNameOrSymbol(char input[]);
 void listAllElements();
